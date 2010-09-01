9070d9598e6e56ab95f4b7d4f2317bb93d42f477
Adam Soltys <adam.soltys@pwgsc-tpsgc.gc.ca>
2010-08-31 20:08:52 -0600

    adding overtime field to time entries and using proper precision for some existing money columns

--- db/migrate/20091230111400_add_invoiced_to_deliverables.rb
+++ db/migrate/20091230111400_add_invoiced_to_deliverables.rb
@@ -1,6 +1,6 @@
 class AddInvoicedToDeliverables < ActiveRecord::Migration
   def self.up
-    add_column :deliverables, :invoiced, :decimal
+    add_column :deliverables, :invoiced, :decimal, :precision => 15, :scale => 2
   end
   
   def self.down
--- db/migrate/20100831084932_add_forecast_to_deliverables.rb
+++ db/migrate/20100831084932_add_forecast_to_deliverables.rb
@@ -1,6 +1,6 @@
 class AddForecastToDeliverables < ActiveRecord::Migration
   def self.up
-    add_column :deliverables, :forecast, :decimal
+    add_column :deliverables, :forecast, :decimal, :precision => 15, :scale => 2
   end
   
   def self.down
